c09557267771e449028425aa3895d727a09c0757
Víctor Dual <vdfuste@gmail.com>
2026-04-08 14:07:10 -0400

    Default new projects directory name changed.

--- src/locations.py
+++ src/locations.py
@@ -6,26 +6,25 @@ UI_PATH:str = os.path.abspath("ui/dist/index.html")
 
 OS_PLATFORM:str = sys.platform
 HOME_DIR:str = os.path.expanduser("~")
-USER_DOCS_DIR:str = os.path.join(HOME_DIR, "Documents")
 
 if OS_PLATFORM == "linux":
 	APP_DIR_NAME = "blenderhub"
 
-	CONFIG_DATA:str = os.path.join("/etc", APP_DIR_NAME, "config.json")
+	#CONFIG_DATA:str = os.path.join("/etc", APP_DIR_NAME, "config.json")
 	INSTALLS_DIR:str = os.path.join("/opt", APP_DIR_NAME, "versions")
 	LOCAL_APP_DATA:str = os.path.join(HOME_DIR, ".local/share", APP_DIR_NAME)
 
 elif "win" in OS_PLATFORM:
 	OS_PLATFORM = "windows"
 
-	CONFIG_DATA:str = os.path.join(os.getenv("PROGRAMDATA"), APP_DIR_NAME, "config.json")
+	#CONFIG_DATA:str = os.path.join(os.getenv("PROGRAMDATA"), APP_DIR_NAME, "config.json")
 	INSTALLS_DIR:str = os.path.join(os.getenv("PROGRAMDATA"), APP_DIR_NAME, "versions")
 	LOCAL_APP_DATA:str = os.path.join(os.getenv("LOCALAPPDATA"), APP_DIR_NAME)
 
 elif OS_PLATFORM == "darwin":
 	OS_PLATFORM = "macos"
 
-	CONFIG_DATA:str = os.path.join("/Library", "Application Support", APP_DIR_NAME, "config.json")
+	#CONFIG_DATA:str = os.path.join("/Library", "Application Support", APP_DIR_NAME, "config.json")
 	INSTALLS_DIR:str = os.path.join("/Applications", APP_DIR_NAME, "versions")
 	LOCAL_APP_DATA:str = os.path.join(HOME_DIR, "Library", "Application Support", APP_DIR_NAME)
 
@@ -33,12 +32,12 @@ else:
 	print("TODO: Make a proper unsupported error message.")
 	sys.exit(1)
 
+USER_DOCS_DIR:str = os.path.join(HOME_DIR, APP_DIR_NAME)
 RELEASES_DATA:str = os.path.join(LOCAL_APP_DATA, "releases.json")
 PROJECTS_DATA:str = os.path.join(LOCAL_APP_DATA, "projects.txt")
 
 if __name__ == "__main__":
 	print(f"OS: {OS_PLATFORM}")
-	print(f"Shared data: {SHARED_DATA}")
 	print(f"Local data: {LOCAL_APP_DATA}")
 	print(f"Documents folder: {USER_DOCS_DIR}")
 	print(f"Blender installs folder: {INSTALLS_DIR}")
